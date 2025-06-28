df690a283a59eab9f64a4b772e30e2d717085671
Padillahirpan <padillahirpan8@gmail.com>
2025-06-28 15:54:12 +0700

    create index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-3xl h-screen flex flex-col items-left gap-4">
+        <section className="flex flex-col items-left gap-4 p-4">
+          <h1 className="text-3xl font-medium text-left">Hi👋</h1>
+          <div className="text-xl font-extralight text-left text-gray-600">
+            I'm Irpan Padillah - Software engineer & android developer who turns
+            complex problems into simple, elegant apps. I love building
+            intuitive experiences in Android and web development. I am always
+            exploring new ideas to make technology feel more human.
+          </div>
+
+          <div className="flex flex-wrap items-center gap-x-3 w-fit">
+            <div>
+              <a
+                href="https://github.com/padillahirpan"
+                target="_blank"
+                className="text-xs md:text-sm text-black border-b border-gray-200 font-medium hover:border-gray-500 transition-all duration-300 ease-in-out"
+              >
+                Github
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://linkedin.com/in/padillahirpan/"
+                target="_blank"
+                className="text-xs md:text-sm text-black border-b border-gray-200 font-medium hover:border-gray-500 transition-all duration-300 ease-in-out"
+              >
+                LinkedIn
+              </a>
+            </div>
+          </div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
